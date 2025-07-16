b0fd52c131aef00ad91f742fd1314e4371324606
ruksanasemeir <ruksana.semeir@tarento.com>
2025-07-16 13:37:04 +0530

    Converting completion percentage to decimal

--- src/main/java/com/igot/cb/access_settings/service/impl/CourseServiceImpl.java
+++ src/main/java/com/igot/cb/access_settings/service/impl/CourseServiceImpl.java
@@ -291,7 +291,31 @@ public class CourseServiceImpl implements CourseService {
         Date inputAccessTime = parseDate((String) inputContent.getOrDefault(Constants.LAST_ACCESS_TIME, ""));
         Object completionPercentage = updatedContent.get(Constants.COMPLETION_PERCENTAGE);
         if (completionPercentage instanceof Integer) {
-            updatedContent.put(Constants.COMPLETION_PERCENTAGE, ((Integer) completionPercentage).doubleValue());
+            double value = ((Integer) completionPercentage).doubleValue();
+            if (value < 0.0 || value > 100.0) {
+                throw new CustomException(
+                        "INVALID_COMPLETION_PERCENTAGE",
+                        "Completion percentage must be between 0 and 100.",
+                        HttpStatus.BAD_REQUEST
+                );
+            }
+            updatedContent.put(Constants.COMPLETION_PERCENTAGE, value);
+        } else if (completionPercentage instanceof Double) {
+            double value = (Double) completionPercentage;
+            if (value < 0.0 || value > 100.0) {
+                throw new CustomException(
+                        "INVALID_COMPLETION_PERCENTAGE",
+                        "Completion percentage must be between 0 and 100.",
+                        HttpStatus.BAD_REQUEST
+                );
+            }
+            updatedContent.put(Constants.COMPLETION_PERCENTAGE, value);
+        } else {
+            throw new CustomException(
+                    "INVALID_COMPLETION_PERCENTAGE_TYPE",
+                    "Completion percentage must be a number.",
+                    HttpStatus.BAD_REQUEST
+            );
         }
         if (existingContent != null && !existingContent.isEmpty()) {
             Date existingAccessTime;

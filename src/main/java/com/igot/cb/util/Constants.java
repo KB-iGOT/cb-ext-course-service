bec53f3b9e2d979be66163486d0974847ab341d9
arpithasureshappa <arpitha.sureshappa@tarento.com>
2025-09-25 00:24:52 +0530

    cbplan karma points API added

--- src/main/java/com/igot/cb/util/Constants.java
+++ src/main/java/com/igot/cb/util/Constants.java
@@ -407,6 +407,7 @@ public class Constants {
     public static final String ATTRIBUTE_NAME = "attributeName";
     public static final String VALUES = "values";
     public static final String VALUE = "value";
+    public static final String X_AUTH_USER_ID = "x-authenticated-userid";
 
 
     private Constants() {

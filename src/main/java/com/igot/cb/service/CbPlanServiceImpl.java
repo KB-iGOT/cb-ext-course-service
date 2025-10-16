7136a4d2c4616ae69fa22c9b193e35e3d0db7005
ruksanasemeir <ruksana.semeir@tarento.com>
2025-10-16 16:44:47 +0530

    Updated PR according to review

--- src/main/java/com/igot/cb/service/CbPlanServiceImpl.java
+++ src/main/java/com/igot/cb/service/CbPlanServiceImpl.java
@@ -563,13 +563,11 @@ public class CbPlanServiceImpl {
                         && Constants.LIVE.equalsIgnoreCase((String) cbPlan.get(Constants.STATUS)))) {
             CbPlanDto cbPlanDto = mapper.readValue((String) cbPlan.get(Constants.DRAFT_DATA), CbPlanDto.class);
             enrichData.put(Constants.NAME, cbPlanDto.getName());
-            enrichData.put(Constants.CONTENT_TYPE, cbPlanDto.getContentType());
             contentTypeInfo = cbPlanDto.getContentList();
             enrichData.put(Constants.END_DATE_REQUEST, cbPlanDto.getEndDate());
             enrichData.put(Constants.IS_APAR, cbPlanDto.getIsApar() != null ? cbPlanDto.getIsApar() : false);
         } else {
             enrichData.put(Constants.NAME, cbPlan.get(Constants.NAME));
-            enrichData.put(Constants.CONTENT_TYPE, cbPlan.get(Constants.CONTENT_TYPE));
             contentTypeInfo = (List<String>) cbPlan.get(Constants.CONTENT_LIST);
             enrichData.put(Constants.END_DATE_REQUEST, cbPlan.get(Constants.END_DATE_REQUEST));
             enrichData.put(Constants.CREATED_AT, cbPlan.get(Constants.CREATED_AT_REQ));
@@ -579,9 +577,9 @@ public class CbPlanServiceImpl {
                         new TypeReference<Map<String, Object>>() {
                         });
                 cbPlanDtoMap.remove(Constants.ID);
-                enrichData.put(Constants.DRAFT_DATA, cbPlanDtoMap);
             }
         }
+        enrichData.put(Constants.CONTENT_TYPE, cbPlan.get(Constants.CONTENT_TYPE));
         enrichData.put(Constants.CREATED_AT, cbPlan.get(Constants.CREATED_AT_REQ));
         enrichData.put(Constants.CB_PUBLISHED_AT, cbPlan.get(Constants.CB_PUBLISHED_AT));
         enrichData.put(Constants.STATUS, cbPlan.get(Constants.STATUS));
@@ -596,7 +594,6 @@ public class CbPlanServiceImpl {
         } else {
             enrichData.put(Constants.CONTEXT_DATA_REQUEST, null); // or skip putting if you prefer
         }
-
         String createdBy = (String) cbPlan.get(Constants.CREATED_BY);
         if (StringUtils.isNotBlank(createdBy)) {
             String createdByUserName = "";

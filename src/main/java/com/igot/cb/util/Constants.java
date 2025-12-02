383e4540a71c0d2d0261035591d3c335ce9471a4
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2025-12-02 10:23:31 +0530

    KB-12170 | BE|DEV| Create new API for listing the Microcredentials content
    1. Caffeine cache was returning empty once cache is expired .
    2. Response structure updated for the content read as per the UI requested.

--- src/main/java/com/igot/cb/util/Constants.java
+++ src/main/java/com/igot/cb/util/Constants.java
@@ -477,6 +477,7 @@ public class Constants {
     public static final String API_PROMOTIONAL_CONTENT_METADATA_UPSERT = "api.promotionalcontent.metadata.upsert";
     public static final String API_PROMOTIONAL_ASSIGNEDTO_USERS = "api.promotionalcontent.assignedto.users";
     public static final String USER_GROUPDETAILS_ERR_VALIDATION_MSG = "User group details cannot be null or empty";
+    public static final String PROGRAM_DURATION = "programDuration";
     private Constants() {
     }
 }

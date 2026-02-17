e25ed99b5bb9038aa4b1b8ccfc295ec6b77c0202
ruksanasemeir <ruksana.semeir@tarento.com>
2026-02-17 12:46:33 +0530

    KB-13086| Q11 | Ad-Hoc | CA and APAR Enhancements | Adding CA flag in APAR Courses

--- src/main/java/com/igot/cb/service/CourseAccessServiceImpl.java
+++ src/main/java/com/igot/cb/service/CourseAccessServiceImpl.java
@@ -185,22 +185,6 @@ public class CourseAccessServiceImpl {
             if (evaluateAccessSettingRule(accessSettingIdMap, userProfile)) {
                 List<String> fieldsToFetch = Arrays.asList(contentReadFields.split(","));
                 Map<String, Object> contentDetails = contentService.readContent(rule.getContextId(), fieldsToFetch);
-                // --- Begin custom logic for courseUnits ---
-                if (contentDetails != null &&
-                    Constants.COURSE_CATEGORY_COMPREHENSIVE_ASSESSMENT_PROGRAM.equals(contentDetails.get(Constants.COURSE_CATEGORY)) &&
-                    contentDetails.get(Constants.CHILD_NODES) instanceof List &&
-                    contentDetails.get(Constants.LEAF_NODES) instanceof List) {
-                    List<String> childNodes = (List<String>) contentDetails.get(Constants.CHILD_NODES);
-                    List<String> leafNodes = (List<String>) contentDetails.get(Constants.LEAF_NODES);
-                    List<String> courseUnits = new ArrayList<>();
-                    for (String node : childNodes) {
-                        if (!leafNodes.contains(node)) {
-                            courseUnits.add(node);
-                        }
-                    }
-                    contentDetails.put(Constants.COURSE_UNITS, courseUnits);
-                }
-                // --- End custom logic for courseUnits ---
                 userCourses.add(contentDetails);
             }
         }
@@ -311,6 +295,22 @@ public class CourseAccessServiceImpl {
                     List<String> fieldsToFetch = Arrays.asList(contentReadFields.split(","));
                     Map<String, Object> contentDetails =
                             contentService.readContent(rule.getContextId(), fieldsToFetch);
+                    // --- Begin custom logic for courseUnits ---
+                    if (contentDetails != null &&
+                            Constants.COURSE_CATEGORY_COMPREHENSIVE_ASSESSMENT_PROGRAM.equals(contentDetails.get(Constants.COURSE_CATEGORY)) &&
+                            contentDetails.get(Constants.CHILD_NODES) instanceof List &&
+                            contentDetails.get(Constants.LEAF_NODES) instanceof List) {
+                        List<String> childNodes = (List<String>) contentDetails.get(Constants.CHILD_NODES);
+                        List<String> leafNodes = (List<String>) contentDetails.get(Constants.LEAF_NODES);
+                        List<String> courseUnits = new ArrayList<>();
+                        for (String node : childNodes) {
+                            if (!leafNodes.contains(node)) {
+                                courseUnits.add(node);
+                            }
+                        }
+                        contentDetails.put(Constants.COURSE_UNITS, courseUnits);
+                    }
+                    // --- End custom logic for courseUnits ---
                     userCourses.add(contentDetails);
                 }
             }

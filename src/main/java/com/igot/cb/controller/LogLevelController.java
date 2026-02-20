541609433ce4179ec2459e4c6c843f32817d9479
Adarsh-m-002 <adarsh.malavade@tarento.com>
2026-02-20 10:44:32 +0530

    added validation on log level

--- src/main/java/com/igot/cb/controller/LogLevelController.java
+++ src/main/java/com/igot/cb/controller/LogLevelController.java
@@ -15,11 +15,15 @@ public class LogLevelController {
     private final LoggingSystem loggingSystem;
 
     @PostMapping("/level")
-    public String changeLogLevel(@RequestParam String level) {
+    public String changeLogLevel(@RequestParam(required = false) String level) {
+
+        if (null == level  || level.trim().isEmpty()) {
+            return "Log level must not be empty. Use DEBUG, INFO, WARN, ERROR";
+        }
 
         LogLevel logLevel;
         try {
-            logLevel = LogLevel.valueOf(level.toUpperCase());
+            logLevel = LogLevel.valueOf(level.trim().toUpperCase());
         } catch (IllegalArgumentException ex) {
             return "Invalid log level... Use DEBUG, INFO, WARN, ERROR";
         }
@@ -31,4 +35,3 @@ public class LogLevelController {
     }
 
 }
-

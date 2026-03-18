3760a3a05ceecf9068604aafaf4e4a2cbcfbf854
ruksanasemeir <ruksana.semeir@tarento.com>
2026-03-18 10:05:37 +0530

    KAR-1167| Issue with isOnCentralDeputation with myAssignedCourse API

--- src/main/java/com/igot/cb/service/UserAndOrgServiceImpl.java
+++ src/main/java/com/igot/cb/service/UserAndOrgServiceImpl.java
@@ -130,10 +130,6 @@ public class UserAndOrgServiceImpl {
                 userList.get(0).put(Constants.PROFILE_DETAILS,  userList.get(0).get(Constants.PROFILE_DETAILS_LOWERCASE));
                 setUserProfile(userProfile, userList.get(0));
             }
-            // Add isOnCentralDeputation as false if not present
-            if (!userProfile.containsKey(Constants.CENTRAL_DEPUTATION)) {
-                userProfile.put(Constants.CENTRAL_DEPUTATION, "false");
-            }
             getUserBitMap(userProfile, userProfileBitMap);
             log.info("User profile fetched for userId: {}, converted bit map profile: {}", userId,
                     mapper.writeValueAsString(userProfileBitMap));

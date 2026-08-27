41fdad7dc4e8ec4505134b37e357a9ad0e2d54e5
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2026-08-27 17:43:03 +0530

    Removed unused param in the method

--- src/main/java/com/igot/cb/cbplan/service/impl/CbPlanServiceV3Impl.java
+++ src/main/java/com/igot/cb/cbplan/service/impl/CbPlanServiceV3Impl.java
@@ -900,8 +900,7 @@ public class CbPlanServiceV3Impl implements CbPlanServiceV3 {
                 (String) profileDetails.get(Constants.PROFILE_STATUS_KEY));
         extractCadreDetails(userProfile, profileDetails);
         enrichmentService.extractMinistryOrStateDetails(userProfile, profileDetails);
-        extractExtendedProfile(userProfile, (String) userBasicProfile.get(Constants.ID),
-                (String) userBasicProfile.get(Constants.ROOT_ORG_ID));
+        extractExtendedProfile(userProfile, (String) userBasicProfile.get(Constants.ID));
     }
 
     private Map<String, Object> parseProfileDetails(Object rawValue) throws JsonProcessingException {
@@ -947,7 +946,7 @@ public class CbPlanServiceV3Impl implements CbPlanServiceV3 {
         userProfile.put(Constants.CENTRAL_DEPUTATION_LOWER_KEY, String.valueOf(centralDeputation));
     }
 
-    private void extractExtendedProfile(Map<String, String> userProfile, String userId, String rootOrgId) {
+    private void extractExtendedProfile(Map<String, String> userProfile, String userId) {
         try {
             Map<String, Object> propertiesMap = Map.of(Constants.USER_ID, userId);
             List<Map<String, Object>> extendedProfileList = cassandraOperation.getRecordsByProperties(

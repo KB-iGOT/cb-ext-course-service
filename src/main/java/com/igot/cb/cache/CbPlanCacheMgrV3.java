c9c937477c014a32a9f7e09e96f2caab3c364c01
tarentomaheshvakkund <mahesh.vakkund@tarento.com>
2026-09-19 14:59:00 +0530

    Revert "KB-15583 | DEV|BE| Enhancement of the User Content Info API - to supp…"
    This reverts commit f9886a758f611a880c66a29b8f04dc2bff930a76.

--- src/main/java/com/igot/cb/cache/CbPlanCacheMgrV3.java
+++ src/main/java/com/igot/cb/cache/CbPlanCacheMgrV3.java
@@ -117,7 +117,7 @@ public class CbPlanCacheMgrV3 {
                     .toList();
             cbPlanCache.put(cacheKey, cbPlanList);
             log.info("getCbPlanForOrgId: Loaded from Cassandra - orgId={}, planYear={}, activeCount={}",
-                    orgId, planYear, cbPlanList.size());
+                    cbPlanList.size(), orgId, planYear);
         } else {
             log.debug("getCbPlanForOrgId: Caffeine cache hit - orgId={}, planYear={}, count={}",
                     orgId, planYear, cbPlanList.size());
